a375e6c3700fbed0130bb436ecfeca86110a02aa
João Victor Carvalho <joaocarvoli@hotmail.com>
2025-03-31 01:39:23 -0300

    feat: client for docker compose

---
+++ src/utils/compose/client.py
@@ -0,0 +1,24 @@
+import os
+from typing import List
+
+from python_on_whales import DockerClient
+
+local_docker_daemon = os.getenv('LOCAL_SERVER_DOCKER_DAEMON')
+remote_docker_daemon = os.getenv("REMOTE_SERVER_DOCKER_DAEMON")
+base_url = remote_docker_daemon if remote_docker_daemon is not None else local_docker_daemon
+
+
+class DockerComposeClient:
+  _instance = None
+
+  def __init__(self):
+    raise RuntimeError("This is a Singleton class, invoke the get_instance() method instead")
+
+  @classmethod
+  def get_instance(cls, compose_files: List[str]):
+    try:
+      if cls._instance is None:
+        cls._instance = DockerClient(host=base_url, compose_files=compose_files)
+      return cls._instance
+    except Exception as e:
+      raise e
